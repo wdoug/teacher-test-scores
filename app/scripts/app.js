9147b130c4a2568d3ff8ec75fafd8da23d21656b
Willy Douglas <willygdouglas@gmail.com>
2014-07-18 21:03:10 -0600

    Yeoman generated angular scaffolding.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name teacherTestScoresApp
+ * @description
+ * # teacherTestScoresApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('teacherTestScoresApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teacherTestScoresApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teacherTestScoresApp
+ */
+angular.module('teacherTestScoresApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
